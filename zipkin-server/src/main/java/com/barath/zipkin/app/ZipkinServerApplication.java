fd33961f9dd08d24d4cc67c7bbf50e6b091861c2
Barath Arivazhagan <barathmacdec91@gmail.com>
2017-10-27 23:31:00 +0530

    code commit

--- zipkin-server/src/main/java/com/barath/zipkin/app/ZipkinServerApplication.java
+++ zipkin-server/src/main/java/com/barath/zipkin/app/ZipkinServerApplication.java
@@ -2,8 +2,12 @@ package com.barath.zipkin.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import zipkin.server.EnableZipkinServer;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableZipkinServer
 public class ZipkinServerApplication {
 
 	public static void main(String[] args) {
